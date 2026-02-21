78072d7733181b57f598126b09eedb96f2c5c464
Mohamed Elbanhawi <128550106+mobanhawi@users.noreply.github.com>
2026-02-21 12:42:34 +1100

    refactor: delete non-functional purgeable space stub for non-Darwin platforms.

--- internal/scanner/purgeable_non_darwin.go
+++
@@ -1,9 +0,0 @@
-//go:build !darwin
-
-package scanner
-
-// GetPurgeableSpace returns the amount of purgeable space in bytes for the volume containing path.
-// On non-darwin platforms, this always returns 0 since purgeable space is a macOS-specific concept.
-func GetPurgeableSpace(path string) int64 {
-	return 0
-}
